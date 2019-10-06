d44393361bb9a4149fee14442c731d0ed9c4d6cd
kieuchicong99 <kieuchicong99@gmail.com>
2019-10-07 06:05:02 +0700

    update code 7/10/2019

--- frontend/App/src/app/module/student/find-tutor.component.ts
+++ frontend/App/src/app/module/student/find-tutor.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { Tutor } from '../../model/Tutor.model';
 
 @Component({
   selector: 'app-find-tutor',
@@ -7,18 +8,43 @@ import { Router } from '@angular/router';
   styleUrls: ['./find-tutor.component.css']
 })
 export class FindTutorComponent implements OnInit {
-  // @Input() title: string;
-  // @Input() detail: string;
-  // @Input() wage: number;
-  // @Input() subject: string;
-  // @Input() address: string;
 
-  title = "Chào mừng đến với abcxyz"
-  detail = "Cần tìm gia sư làm chuyện ấy, nam nữ không quan trọng. Địa chỉ số xyz Duy HƯng Chan ..."
-  wage = 10000
-  subject = "Chuyện khó nói  "
-  address = "Hà Nội  "
-  require ="Gặp mặt"
+  @Output() tutor;
+  ngOnInit() {
+  }
+
+  public tutor_kind =["Gia sư tiếng anh", "Gia sư toán", "Gia sư văn", "Gia sư vật lý"]
+
+  public tutors:Array<Tutor> =[{
+    "title" : "Tìm gia sư dạy toán tiểu học",
+    "detail" : "Cần tìm gia sư cho 2 bé tuổi, dạy viết, luyện chữ ...",
+    "wage" : 10000,
+    "subject" : "Chuyện khó nói  ",
+    "address" : "Hà Nội  ",
+    "require" :"Gặp mặt"
+
+  },
+  {
+    "title" : "Tìm gia sư luyện thi đại học",
+    "detail" : "Việc làm 123 đăng tin giúp Gia đình ở khu vực 136 hồ tùng mậu cần tìm gia sư toán. Tiếng anh. Tiếng trung cho con gái lớp 5",
+    "wage" : 10000,
+    "subject" : "Chuyện khó nói  ",
+    "address" : "Hà Nội  ",
+    "require" :"Gặp mặt"
+
+  },
+  {
+    "title" : "Cần tìm gia sư toán lớp 2 tại Quảng Ninh",
+    "detail" : "Việc làm 123 đăng tin giúp cần tìm gia sư toán cho bé lớp 2. Địa chỉ Cẩm Phả, Quảng Ninh. Bé học hơi kém môn toán lên cần tìm gấp ạ. Không mất phí",
+    "wage" : 10000,
+    "subject" : "Chuyện khó nói  ",
+    "address" : "Quảng Ninh",
+    "require" :"Gặp mặt"
+
+  }
+]
+
+
 
   constructor(public router:Router ) { }
 
@@ -26,7 +52,5 @@ export class FindTutorComponent implements OnInit {
     this.router.navigateByUrl('tim-lop-gia-su/chitiet');
   }
 
-  ngOnInit() {
-  }
 
 }
