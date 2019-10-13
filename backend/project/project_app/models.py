c11af182104f635f06bdf49e50a5cb9b161a9078
phonguet <40883370+phonguet@users.noreply.github.com>
2019-10-13 11:28:08 +0700

    Update models.py

--- backend/project/project_app/models.py
+++ backend/project/project_app/models.py
@@ -3,6 +3,8 @@ from datetime import date, datetime, timedelta
 from django.db.models import Avg
 
 # Create your models here.
+
+# User
 class User(models.Model):
     name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200, blank=True)
@@ -32,3 +34,60 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+# Tutor
+class Tutor(PolymorphicModel):
+    user = models.OneToOneField(User)
+    course = models.ManyToManyField(Course, blank=True)
+    shortBio = models.CharField(max_length=300)
+    rating = models.FloatField(default=0)
+    subject_tags = models.ManyToManyField(Tag, blank=True)
+    isActivated = models.BooleanField(default=True)
+
+    def create_unavailable_slot(self, date, time_start):
+        pass
+
+    def remove_unavailable_slot(self, date, time_start):
+        UnavailableSlot.objects.get(tutor=self, date=date, time_start=time_start).delete()
+
+    def add_course(self, courseCode):
+        c = Course.objects.get(code=courseCode)
+        self.course.add(c)
+        self.save()
+
+    def remove_course(self, courseCode):
+        c = Course.objects.get(code=courseCode)
+        self.course.remove(c)
+        self.save()
+
+    def activate_deactivate(self):
+        if self.isActivated:
+            self.isActivated = False
+            self.save()
+        else:
+            self.isActivated = True
+            self.save()
+
+    def add_tag(self, tagName, create):
+        if create:
+            t = Tag(tag_name=tagName)
+            t.save()
+
+        t2 = Tag.objects.get(tag_name=tagName)
+        self.subject_tags.add(t2)
+        self.save()
+
+    def remove_tag(self, tagName):
+        t = Tag.objects.get(tag_name=tagName)
+        self.subject_tags.remove(t)
+        self.save()
+
+    def update_rating(self):
+        newRating = Review.objects.filter(tutor=self).aggregate(Avg('rating'))
+        print(newRating)
+        setattr(self, 'rating', newRating['rating__avg'])
+        self.save()
+
+    def __str__(self):
+        return self.user.name
+
