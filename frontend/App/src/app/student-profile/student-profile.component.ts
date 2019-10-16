1d073ac6eed664c5bfcb7dd5c6317c2ab4834db6
NamDuongkiwi <43265144+NamDuongkiwi@users.noreply.github.com>
2019-10-17 02:23:24 +0700

    student-profile

---
+++ frontend/App/src/app/student-profile/student-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-profile',
+  templateUrl: './app-student-profile.html',
+  styleUrls: ['./app-student-profile.css']
+})
+export class DemoProfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
