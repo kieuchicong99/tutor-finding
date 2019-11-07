4531d803051f2ee42b94875fb7a9ee2cf8603570
ThanhThuUet <thanhthu97uet@gmai.com>
2019-11-08 03:56:50 +0700

    Update table

--- frontend/App/src/app/module/manage-acc/manage-acc/table/table.component.ts
+++ frontend/App/src/app/module/manage-acc/manage-acc/table/table.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+dataTable  = [
+	{name:'Superman', subject:'Toán', method:'Tại nhà', cost:'200.000/buổi', date:'06.11.2019', status:'Waiting'},
+	{name:'Batman', subject:'Lý', method:'Online', cost:'200.000/buổi', date:'06.11.2019', status:'Waiting'},
+	{name:'BatGirl', subject:'Hóa', method:'Tại nhà', cost:'200.000/buổi', date:'06.11.2019', status:'Waiting'},
+	{name:'Robin', subject:'Anh', method:'Tại nhà', cost:'200.000/buổi', date:'06.11.2019', status:'Waiting'},
+	{name:'Flash', subject:'Văn', method:'Online + Tại nhà', cost:'200.000/buổi', date:'06.11.2019', status:'Waiting'}
+	];
+
   constructor() { }
 
   ngOnInit() {
---
+++ frontend/App/src/assets/table.js
@@ -0,0 +1,3 @@
+$(document).on("click", ".deleteIcon", function() {
+$(this).closest('tr').remove();
+});
