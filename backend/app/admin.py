c200913d786597ff6479c0292ee069212916975c
NaTaShaRMN <tuananhnguyenyt@gmail.com>
2019-12-22 00:13:49 +0700

    init admin site

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -4,5 +4,6 @@ from .models import Binh_luan, Danh_gia, Danh_sach_lop, Gia_su, Level, Lop_yeu_c
 admin.site.register(Binh_luan)
 admin.site.register(Danh_gia)
 admin.site.register(Danh_sach_lop)
+admin.site.register(Gia_su)
 
 
