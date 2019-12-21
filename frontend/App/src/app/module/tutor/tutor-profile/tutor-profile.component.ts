4fb249af80df9e88cb514af015a4ae2c9b196a0d
ThanhThuUet <thanhthu97uet@gmai.com>
2019-12-21 22:57:45 +0700

    update tutor-profile

--- frontend/App/src/app/module/tutor/tutor-profile/tutor-profile.component.ts
+++ frontend/App/src/app/module/tutor/tutor-profile/tutor-profile.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service : DefaultService) {}
+
+  content = "Toi da co bang ABRSM Grade 8 Piano nhung toi chua co kinh nghiem di day co gi phu huynh va hoc sinh thong cam giup toi!!!!!"
+
+  public profile_tutor={
+    name:"Tran Hoang Phuong",
+    subject:"Tieng Anh",
+    
+
+  }
 
   ngOnInit() {
   }
