5afe80723f73e67bf364aaed92ad3f3e7947500b
NaTaShaRMN <tuananhnguyenyt@gmail.com>
2019-12-22 00:12:51 +0700

    init admin site

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import Binh_luan, Danh_gia, Danh_sach_lop, Gia_su, Level, Lop_yeu_cau, Lop_yeu_cau_Gia_su, Mon_hoc, Phu_huynh, So_lien_lac
 # Register your models here.
 admin.site.register(Binh_luan)
+admin.site.register(Danh_gia)
 
 
