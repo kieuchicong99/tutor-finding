7370758be251aaf05ea48ab26e7e82c525baa5ca
kieuchicong99 <kieuchicong99@gmail.com>
2019-12-21 10:01:14 +0700

    update api update tutor

--- backend/app/api/api_tutor.py
+++ backend/app/api/api_tutor.py
@@ -14,7 +14,7 @@ from rest_framework_jwt.settings import api_settings
 
 from app.models import Gia_su
 from app.serializers.ser_tutor import SerTutors, SerTutor
-
+import jwt
 
 
 
@@ -135,53 +135,139 @@ class Tutor(APIView):
 
 class UpdateTutor(APIView):
     # view for TuTor
+
     schema=ManualSchema(fields=[
         coreapi.Field(
-            "ho_ten",
+            "id_gia_su",
             required=True,
+            location="form",
+            schema=coreschema.String()
+        ),
+        coreapi.Field(
+            "ho_ten",
+            # required=True,
             location="form",
             schema=coreschema.String()
         ),
         coreapi.Field(
             "email",
-            required=True,
+            # required=True,
             location="form",
             schema=coreschema.String()
         ),
          coreapi.Field(
             "mat_khau",
-            required=True,
+            # required=True,
             location="form",
             schema=coreschema.String()
         ),
          coreapi.Field(
             "hinh_dai_dien_url",
-            required=True,
+            # required=True,
             location="form",
             schema=coreschema.String()
-        )])
+        ),
+        coreapi.Field(
+            "gioi_thieu",
+            # required=True,
+            location="form",
+            schema=coreschema.String()
+        ),
+         coreapi.Field(
+            "gioi_tinh",
+            # required=True,
+            location="form",
+            schema=coreschema.String()
+        ),
+         coreapi.Field(
+            "dia_chi",
+            # required=True,
+            location="form",
+            schema=coreschema.String()
+        ),
+         coreapi.Field(
+            "hoc_phi_gs",
+            # required=True,
+            location="form",
+            schema=coreschema.String()
+        ),
+         coreapi.Field(
+            "hinh_thuc_day",
+            # required=True,
+            location="form",
+            schema=coreschema.String()
+        ),
+         coreapi.Field(
+            "kinh_nghiem",
+            # required=True,
+            location="form",
+            schema=coreschema.String()
+        ),
+         coreapi.Field(
+            "buoi_day",
+            # required=True,
+            location="form",
+            schema=coreschema.String()
+        ),
+        ])
     def post(self, request):
-        ho_ten=request.data['ho_ten']
-        email=request.data['email']
-        mat_khau=request.data['mat_khau']
-        hinh_dai_dien_url=request.data['hinh_dai_dien_url']
+        id_gia_su= request.data['id_gia_su']
+        tutor=Gia_su.objects.get(id_gia_su=id_gia_su)
+        old_tutor =  Gia_su.objects.get(id_gia_su=id_gia_su)
+        print("request data:", request.data)
 
-        
-        tutor=Gia_su.objects.get(id_gia_su=1)
-        old_tutor =  Gia_su.objects.get(id_gia_su=1)
-        tutor.ho_ten= ho_ten
-        tutor.email = email
-        tutor.mat_khau = mat_khau
-        tutor.hinh_dai_dien_url = hinh_dai_dien_url
+        if ("ho_ten" in request.data and(request.data['ho_ten']!='')) :
+            ho_ten=request.data['ho_ten']
+            tutor.ho_ten= ho_ten
+              
+        if ("email" in request.data and(request.data['email']!='')):
+            email=request.data['email']
+            tutor.email = email
 
-        
+        if ("mat_khau" in request.data and(request.data['mat_khau']!='')):
+            mat_khau=hashers.SHA1PasswordHasher().encode(request.data['mat_khau'],salt='123')
+            tutor.mat_khau = mat_khau
+
+        if ("hinh_dai_dien_url" in request.data and(request.data['hinh_dai_dien_url']!='')):
+            hinh_dai_dien_url=request.data['hinh_dai_dien_url']
+            tutor.hinh_dai_dien_url = hinh_dai_dien_url
+
+        if  ("gioi_thieu" in request.data and(request.data['gioi_thieu']!='')):
+            gioi_thieu = request.data['gioi_thieu']
+            tutor.gioi_thieu= gioi_thieu
+
+        if  ("gioi_tinh" in request.data and(request.data['gioi_tinh']!='')):
+            gioi_tinh = request.data['gioi_tinh']
+            tutor.gioi_tinh = gioi_tinh
+
+        if ("dia_chi" in request.data and(request.data['dia_chi']!='')):
+            dia_chi= request.data['dia_chi']
+            tutor.dia_chi= dia_chi
+
+        if ("hoc_phi_gs" in request.data and(request.data['hoc_phi_gs']!='')):
+            hoc_phi_gs = request.data['hoc_phi_gs']
+            tutor.hoc_phi_gs = hoc_phi_gs
+
+        if ("hinh_thuc_day" in request.data and(request.data['hinh_thuc_day']!='')):
+            hinh_thuc_day= request.data['hinh_thuc_day']
+            tutor.hinh_thuc_day= hinh_thuc_day
+
+        if ("kinh_nghiem" in request.data and(request.data['kinh_nghiem']!='')):
+            kinh_nghiem = request.data['kinh_nghiem']
+            tutor.kinh_nghiem = kinh_nghiem
+
+        if ("buoi_day" in request.data and(request.data['buoi_day']!='')):
+            buoi_day = request.data['buoi_day']
+            tutor.buoi_day = buoi_day    
+                    
+            
         try:
             try:
                 tutor.save()
                 return Response({"success": True, "message": "Cập nhập thành công ", "old_value":SerTutor(old_tutor).data, "new_value":SerTutor(tutor).data}, status.HTTP_200_OK)
             except Exception as e:
                 print("Error:", e)
-                return Response({"success": False, "message": "Số điện thoại đã tồn tại"}, status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response({"success": False, "message": e}, status.HTTP_500_INTERNAL_SERVER_ERROR)
         except Exception as e:
             print("Error:", e)
             return Response({"success": False, "message": "Lỗi hệ thống"}, status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -212,7 +298,7 @@ class TutorListPage(APIView):
         Trả về danh sách theo page
         """
         tutor = Gia_su.objects.all()
-        paginator = Paginator(tutor,10)
+        paginator = Paginatand(tutor,10)
         listTutor= paginator.get_page(page)
         listTutorSerializer = SerTutor(listTutor, many=True)
 
