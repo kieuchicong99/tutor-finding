03a7d94e2d508fee6623019bf9c3ca70e4e828f5
ThanhThuUet <thanhthu97uet@gmai.com>
2019-12-21 21:04:54 +0700

    tutor-profile

--- frontend/App/src/app/module/tutor/tutor-profile/tutor-profile.component.ts
+++ frontend/App/src/app/module/tutor/tutor-profile/tutor-profile.component.ts
@@ -9,6 +9,9 @@ import { DefaultService } from '../../../service/default.service';
 export class TutorProfileComponent implements OnInit {
 
   constructor(public service : DefaultService) {}
+
+  content = "Toi da co bang ABRSM Grade 8 Piano nhung toi chua co kinh nghiem di day co gi phu huynh va hoc sinh thong cam giup toi!!!!!"
+
   public profile_tutor={
     name:"Tran Hoang Phuong",
     subject:"Tran Hoang Phuong",
