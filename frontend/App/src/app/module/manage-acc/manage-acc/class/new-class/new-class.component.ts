eaa823dc748b8484344fd9db425ee094619ceb48
kieuchicong99 <kieuchicong99@gmail.com>
2019-12-24 00:50:30 +0700

    add initialization for newclass

--- frontend/App/src/app/module/manage-acc/manage-acc/class/new-class/new-class.component.ts
+++ frontend/App/src/app/module/manage-acc/manage-acc/class/new-class/new-class.component.ts
@@ -9,50 +9,20 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class NewClassComponent implements OnInit {
 
-  constructor(public service: ClassService, public toastr: ToastrService) { }
-
-  // public new_class = {
-  //   demand_content: '',
-  //   type_tutor: '',
-  //   subject: '',
-  //   sex_tutor: '',
-  //   num_student: 1,
-  //   method: '',
-  //   time_for_one: '',
-  //   schedule: '',
-  //   cost: '',
-  //   address: '',
-  //   address_detail: '',
-  //   description: '',
-  //   phone: '',
-  //   email: '',
-  // }
-
-  // public type_tutors = [
-  //   { key: "none", value: "--Không yêu cầu--" },
-  //   { key: "sinh_vien", value: "Sinh viên" },
-  //   { key: "hoc_sinh", value: "Học sinh" },
-  //   { key: "giao_vien_c1", value: "Giáo viên cấp 1 " },
-  //   { key: "giao_vien_c2", value: "Giáo viên cấp 2" },
-  //   { key: "giao_vien_c3", value: "Giáo viên cấp 3" },
-  //   { key: "giang_vien", value: "Giảng viên đại học" },
-  // ]
-
-  // public subjects = [
-  //   { key : "toan", value : "Toán"},
-  //   { key : "ly", value : "Lý"},
-  //   { key : "hoa", value : "Hóa"},
-  //   { key : "van", value : "Văn"},
-  //   { key : "sinh", value : "Sinh"},
-  //   { key : "su", value : "Sử"},
-  //   { key : "dia", value : "Địa"},
-  //   { key : "tieng_anh", value : "Tiếng anh"},
-  // ]
-
-  // public addresses = [
-  //   { key : "ha_noi", value : "Hà Nội"},
-  //   { key : "ho_chi_minh", value : "Hồ Chí Minh"},
-  // ]
+  constructor(public service: ClassService, public toastr: ToastrService) {
+    this.new_class =  {
+      id_phu_huynh: '',
+      trang_thai:'',
+      mo_ta: '',
+      mon_hoc: '',
+      hinh_thuc_day: '',
+      yeu_cau_gioi_tinh: '',
+      so_buoi_hoc_moi_tuan: '',
+      so_gio_moi_buoi: '',
+      gia_thue_moi_buoi: '',
+      dia_chi: '',
+    }
+  }
 
   public new_class
 
@@ -60,15 +30,11 @@ export class NewClassComponent implements OnInit {
     await this.service.createClass(this.new_class).then(res => {
       console.log("res222:", res)
       if (res === true) {
-        this.toastr.success('Tạo lớp thành công!', 'ddd  ', {
-          timeOut: 3000
-        });
+        this.toastr.success('Tạo lớp thành công!');
 
       }
       if (res === false) {
-        this.toastr.success('Tạo lớp thất bại!', ' ddd  ', {
-          timeOut: 3000
-        });
+        this.toastr.success('Tạo lớp thất bại!');
 
       }
     })
